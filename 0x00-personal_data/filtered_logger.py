a92672fe683f056a7869818537a710887aaf7780
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-04-14 00:01:12 +0100

    Add initial files for personal data handling

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""filtered_logger"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """ Replacing """
+    for f in fields:
+        message = re.sub(rf"{f}=(.*?)\{separator}",
+                         f'{f}={redaction}{separator}', message)
+    return message
