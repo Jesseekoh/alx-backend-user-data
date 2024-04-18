35a5c7607d75da95fba62f9e751504448cf11cc7
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-04-18 22:30:03 +0100

    Refactor require_auth method in Auth class for improved code logic and readability

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """auth module"""
-from typing import List, TypeVar
+from typing import TypeVar
 
 from flask import request
 
@@ -9,13 +9,11 @@ class Auth:
     """Auth class definition"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """require authentication"""
+        """require auth"""
         return False
 
     def authorization_header(self, request=None) -> str:
-        # if not request and "Authorization" not in request.header:
         return None
-        # return request.header["Authorization"]
 
     def current_user(self, request=None) -> TypeVar('User'):
         return None
