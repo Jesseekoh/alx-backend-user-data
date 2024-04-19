d6e5added07e46825ac67fae3d026bcd5e3806ef
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-04-19 22:15:10 +0100

    Refactor decode_base64_authorization_header method in BasicAuth class for improved code logic and readability

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """basic auth module"""
+import base64
+import binascii
 import re
 
 from .auth import Auth
@@ -8,8 +10,9 @@ from .auth import Auth
 class BasicAuth(Auth):
     """BasicAuth class definition"""
 
-    def extract_base64_authorization_header(self,
-                                            authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
         """Extracts the Base64 part of the Authorization header
         for a Basic Authentication.
         """
@@ -19,3 +22,18 @@ class BasicAuth(Auth):
             if field_match is not None:
                 return field_match.group('token')
         return None
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """Decodes a base64-encoded authorization header
+        """
+        if type(base64_authorization_header) == str:
+            try:
+                res = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True,
+                )
+                return res.decode('utf-8')
+            except (binascii.Error, UnicodeDecodeError):
+                return None
