d54dac38ecfc48f520e1e977d8b0bfb7eb92e709
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-04-20 11:35:36 +0100

    Refactor delete_user function in users.py to return current user's JSON representation when user_id is 'me'

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -47,9 +47,10 @@ def delete_user(user_id: str = None) -> str:
         abort(404)
     if user_id == 'me' and request.current_user is None:
         abort(404)
+
     user = User.get(user_id)
     if user_id == 'me' and request.current_user is not None:
-        return jsonify(user.to_json())
+        return jsonify(request.current_user.to_json())
     user = User.get(user_id)
     if user is None:
         abort(404)
